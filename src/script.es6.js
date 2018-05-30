29df19f865163b0ca552b93189f5016c4e966d36
Ashok Mandal <askipop@gmail.com>
2018-05-30 15:23:16 +0530

    added maps, realtimearea

--- src/script.es6.js
+++ src/script.es6.js
@@ -1,2 +1,6 @@
-import render from "../chart-scripts/pie2d-render";
+// Uncomment code to render select chart
+// import render from '../chart-scripts/pie2d-render';
+// import render from '../chart-scripts/column2d-render';
+// import render from '../chart-scripts/realtimearea-render';
+// import render from '../chart-scripts/mapusa-render';
 render.render();
