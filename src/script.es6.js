8373ab6ca37bfd782fefc7471b3ead072f7235b0
Ashok Mandal <askipop@gmail.com>
2018-06-01 12:32:03 +0530

    Added script for rollup

--- src/script.es6.js
+++ src/script.es6.js
@@ -1,5 +1,5 @@
 // Uncomment code to render select chart
-// import render from '../chart-scripts/pie2d-render';
+import render from '../chart-scripts/pie2d-render';
 // import render from '../chart-scripts/column2d-render';
 // import render from '../chart-scripts/realtimearea-render';
 // import render from '../chart-scripts/mapusa-render';
